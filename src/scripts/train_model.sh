e742c2ef65b5c9a9f8b65fc17c7bb122030b7481
Mark Lyubimov <mark.lyubimov@ostrovok.ru>
2020-05-10 23:40:29 +0300

    train model updated + sh

--- src/scripts/train_model.sh
+++ src/scripts/train_model.sh
@@ -1,3 +1,3 @@
-python /src/models/train_model.py --sequence_length 5 10 50 100 400
-python /src/models/train_model.py --sequence_length 400
-python /src/models/train_model.py --sequence_length 5
+python src/models/train_model.py --sequence_length 5 10 50 100 400
+python src/models/train_model.py --sequence_length 400
+python src/models/train_model.py --sequence_length 5
